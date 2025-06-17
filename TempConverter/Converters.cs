cf02146ab883915d7632497c0e3076608b6ac24b
Nelly <Nelly.sosobrado@gmail.com>
2025-06-17 16:48:58 +0200

    Class for temperature

--- TempConverter/Converters.cs
+++ TempConverter/Converters.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TempConverter
 {
-    internal class Converters
+    public class Converters
     {
+
+        public double f2c(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+        public double c2f(double c)
+        {
+            return (c * 9 / 5) + 32;
+        }
+
     }
+
 }
