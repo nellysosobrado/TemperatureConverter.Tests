eec14d1c0fe9c27af09ee3419e2aa19893a1fe23
Nelly <Nelly.sosobrado@gmail.com>
2025-06-17 17:05:51 +0200

    Reference to SUT project.

---
+++ TempConverter.Tests/ConvertTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter.Tests
+{
+    [TestClass]
+
+    public class ConvertTests
+    {
+        private readonly Converters _sut;
+        public ConvertTests()
+        {
+            _sut = new Converters();
+        }
+
+        [TestMethod]
+        public void Check_F2C()
+        {
+        }
+    }
+
+}
